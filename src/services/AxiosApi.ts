b1cd9ef6c1748dd9620c463952d86d20b6b610e0
Your GitHub Username <Your GitHub Email>
2025-06-23 15:32:41 +0300

    ll

--- src/services/AxiosApi.ts
+++ src/services/AxiosApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getToken, removeToken } from "../utils/helper";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = `${import.meta.env.VITE_API_URL}/api`;
 
 export const api = axios.create({
   baseURL: API_URL,
